c10cbb5ccf702a886037c00da34a247124ebc35e
Matt Norton <50421104+norto2mr@users.noreply.github.com>
2023-12-11 16:09:46 -0500

    Refactor loadRank

--- loadRank.js
+++ loadRank.js
@@ -1,27 +1,25 @@
-const url =
-  "https://crossorigin.me/" +
-  encodeURIComponent("https://www.arinsights.com/architect-power-100/");
+const url = "https://corsproxy.org/?" + encodeURIComponent("https://www.arinsights.com/architect-power-100/");
+const response = await fetch(url);
+const html = await response.text();
+
+function loadRanks(html) {
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(responseText, "text/html");
+
+  const Katie = Array.from(doc.querySelectorAll(".col3"))
+    .find((el) => el.textContent === "Katie Norton")
+    .closest("section")
+    .querySelector(".col1")
+    .innerText;
+  
+  const Jim = Array.from(doc.querySelectorAll(".col3"))
+    .find((el) => el.textContent === "Jim Mercer")
+    .innerText;
+
+  document.querySelector("#analystRankKatie").innerText = Katie;
+  document.querySelector("#analystRankJim").innerText = Jim;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  fetch(url).then(function (response) {
-    response.text().then(function (responseText) {
-      var parser = new DOMParser();
-
-      // Parse the text
-      var doc = parser.parseFromString(responseText, "text/html");
-      console.log(doc);
-      console.log(doc.querySelectorAll(".col3"));
-      const Katie = Array.from(doc.querySelectorAll(".col3"))
-        .find((el) => el.textContent === "Katie Norton")
-        .closest("section")
-        .querySelector(".col1")
-        .innerText;
-      
-      const Jim = Array.from(doc.querySelectorAll(".col3"))
-        .find((el) => el.textContent === "Jim Mercer")
-        .innerText;
-
-      document.querySelector("#analystRankKatie").innerText = Katie;
-      document.querySelector("#analystRankJim").innerText = Jim;
-    });
-  });
+  loadRanks(html);
 });
